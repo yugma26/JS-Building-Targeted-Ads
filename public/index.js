75189e65b2b9cee5b8a1f56162fc22be15722d77
yugma26 <66039476+yugma26@users.noreply.github.com>
2022-10-17 20:41:38 -0400

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,22 @@
+async function main(){
+    const countContainer = document.querySelector('#count-container');
+    const incrementButton = document.querySelector('#increment-button');
+    const decrementButton = document.querySelector('#decrement-button');
+
+    let countValue = await fetch("http://192.168.1.147:9001/counter");
+
+    function increment(){
+        countValue++;
+        countContainer.textContent = countValue;
+    }
+
+    function decrement(){
+        countValue--;
+        countContainer.textContent = countValue;
+    }
+
+    incrementButton.addEventListener('click', increment);
+    decrementButton.addEventListener('click', decrement);
+    countContainer.textContent = countValue;
+}
+main()
